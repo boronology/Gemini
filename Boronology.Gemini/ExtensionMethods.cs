85c2652416c5a753d7a00698ef7d309d4d18b1a8
boronology <boronology@gmail.com>
2021-09-10 22:08:05 +0900

    単純な値型に対応

--- Boronology.Gemini/ExtensionMethods.cs
+++ Boronology.Gemini/ExtensionMethods.cs
@@ -6,7 +6,57 @@ namespace Boronology.Gemini
     {
         public static object DeepClone (this object value)
         {
-            throw new NotImplementedException ();
+            if (object.Equals(value, null))
+            {
+                return null;
+            }
+            Type type = value.GetType();
+            return InternalClone(type, value);
         }
+
+        private static object InternalClone(Type type, object value)
+        {
+            if (type.IsValueType)
+            {
+                if (type.IsEnum)
+                {
+                    return value;
+                }
+                else if (type.IsPrimitive)
+                {
+                    //プリミティブ型はそのまま返せる
+                    return value;
+                }
+                else if (type == typeof(decimal))
+                {
+                    return value;
+                }
+                else
+                {
+                    //再帰的なCloneが必要
+                    throw new NotImplementedException();
+                }
+            }
+            else if (type.IsClass)
+            {
+                if (type == typeof(string))
+                {
+                    string str = value as string;
+
+                    //DeepCloneしてもインターン文字列になる可能性があるのでそのまま返しておく
+                    return value;
+                }
+                else
+                {
+                    //再帰的なCloneが必要
+                    throw new NotImplementedException();
+                }
+            }
+            else
+            {
+                throw new ArgumentException($"typeは具体的な型のはず。type = {type.Name}, value = {value}");
+            }
+        }
+
     }
 }
